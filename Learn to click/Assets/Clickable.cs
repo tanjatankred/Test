5179fd328fba513dc6f06598c30435a2339b8922
tanjatankred <tanja.tankred@gmail.com>
2016-02-13 18:00:53 +0100

    I made a project!

---
+++ Learn to click/Assets/Clickable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clickable : MonoBehaviour {
+
+	public Animator animator;
+
+	public bool playAnimation = false;
+
+	void Start (){
+		animator = GetComponent<Animator> ();
+	
+	}
+
+	void OnMouseDown() { 
+		playAnimation = !playAnimation;
+		animator.SetBool ("Animate", playAnimation);
+		Debug.Log ("uuuuh");
+	
+	}
+
+
+}
